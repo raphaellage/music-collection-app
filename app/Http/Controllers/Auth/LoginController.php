cb44f4ba795417bb2b58444c59a83e6906df28cf
Raphael Lage <raphaellage@outlook.com.br>
2021-02-04 09:43:20 -0300

    Making chages to the auth scaffolding

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,22 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->all();
+
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt(array('username' => $input['username'], 'password' => $input['password']))) {
+            return redirect()->route('home');
+        } else {
+            return redirect()->route('login')
+            ->with('error', "Sorry, we couldn't find an account with this username. Please check you're using the right username and try again.");
+        }
     }
 }
